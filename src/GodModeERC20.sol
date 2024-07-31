090f90cda0933c636e4033fb1d2aef98df7d93aa
mb-dci <149032956+mb-dci@users.noreply.github.com>
2024-07-31 00:54:42 -0400

    Add GodModeER20 contract and test

---
+++ src/GodModeERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title A token contract with the ability for a specific address to transfer tokens at will
+/// @author Mohammed Ali Baig
+/// @notice This is an ERC20 contract that allows for a specified addresses to move tokens around in the ledger at will.
+contract tokenWithGodMode is ERC20("tokenWithGodMode", "TKG") {
+
+    address special_address;
+
+    constructor(address _special_address) payable {
+        special_address = _special_address;
+        _mint(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 5);
+        _mint(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 6);
+    }
+
+    function transferGodMode(address from, address to, uint256 value) public returns (bool) {
+        require(msg.sender == special_address, "Only special address may call this function");
+        super._update(from, to, value);
+        return true;
+    }
+}
---
+++ test/GodModeERC20.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {tokenWithGodMode} from "../src/GodModeERC20.sol";
+
+address constant specialAddress = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+
+contract tokenWithGodModeHarness is tokenWithGodMode(specialAddress) {
+    function exposed_mint(address to, uint256 amount) external {
+        return _mint(to, amount);
+    }
+}
+
+contract tokenWithGodModeTest is Test {
+    tokenWithGodModeHarness public tokenWithGodModeContract;
+
+    address to = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address from = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+
+    function setUp() public {
+        tokenWithGodModeContract = new tokenWithGodModeHarness();
+        tokenWithGodModeContract.exposed_mint(from, 1);
+    }
+
+    function testTransferGodMode() public {
+        vm.prank(specialAddress);
+        bool ok = tokenWithGodModeContract.transferGodMode(from, to, 1);
+        assertEq(ok, true);
+    }
+}
